edfc208aae27530d1b6dcfcc20f8e539413a43d9
exoego <mogami@exoego.net>
2018-04-14 08:28:53 +0900

    Separate outDir for testing so one can verify result for each versions.

--- test/index.js
+++ test/index.js
@@ -12,19 +12,19 @@ const appRoot = require('app-root-path');
 describe('sis', () => {
 
   const swaggerVersions = [
-      {"name": "Swagger 2.0 (legacy)", "specFile":"test/swagger.yaml"},
-      {"name": "OpenAPI 3.0", "specFile":"test/openapi30.yaml"}
+      {"name": "Swagger 2.0 (legacy)", "specFile":"test/swagger.yaml", "outDir":"test/src/swagger20"},
+      {"name": "OpenAPI 3.0", "specFile":"test/openapi30.yaml", "outDir":"test/src/openapi30"}
   ];
 
   swaggerVersions.forEach(swaggerVersion => {
     describe(`${swaggerVersion.name}: interface testing`, () => {
       before(() => {
-        const command = `${appRoot.path}/bin/sis -i ${swaggerVersion.specFile} -c test/serverless.common.yml -S sis -o test/src -f -O -B`;
+        const command = `${appRoot.path}/bin/sis -i ${swaggerVersion.specFile} -c test/serverless.common.yml -S sis -o ${swaggerVersion.outDir} -f -O -B`;
         childProcess.execSync(command);
       });
 
       it('Should generate authentication service.', (done) => {
-        fs.readdir(path.resolve(appRoot.path, './test/src/authentication'), (error, results) => {
+        fs.readdir(path.resolve(appRoot.path, `${swaggerVersion.outDir}/authentication`), (error, results) => {
           assert.ok(results.some((result) => {
             return (result === 'serverless.yml');
           }));
@@ -38,7 +38,7 @@ describe('sis', () => {
       });
 
       it('Should generate test service.', (done) => {
-        fs.readdir(path.resolve(appRoot.path, './test/src/test'), (error, results) => {
+        fs.readdir(path.resolve(appRoot.path, `${swaggerVersion.outDir}/test`), (error, results) => {
           assert.ok(results.some((result) => {
             return (result === 'serverless.yml');
           }));
@@ -52,7 +52,7 @@ describe('sis', () => {
       });
 
       after(() => {
-        return del(path.resolve(appRoot.path, './test/src'));
+        return del(path.resolve(appRoot.path, swaggerVersion.outDir));
       });
     });
   });
